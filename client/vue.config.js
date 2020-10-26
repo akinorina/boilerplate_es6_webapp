576388ba1aebda4cfa0481bbbd9234442d135858
Akinori NAKATA <akinori.na@gmail.com>
2020-10-26 23:52:36 +0900

    client - APIレスポンス変更対応、認証処理の整理。

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,13 @@ module.exports = {
   configureWebpack: {
     devServer: {
       proxy: 'http://boilerplate-es6-webapp.local'
+    },
+    performance: {
+      maxEntrypointSize: 1000000,
+      maxAssetSize: 1000000,
+      assetFilter: function assetFilter(assetFilename) {
+        return !(/\.map$/.test(assetFilename));
+      }
     }
   }
 }
